c318a5d5972678a41dd8085bb8382e400f4ffd7e
Thomas Blitz <tbl@infomedia.dk>
2014-12-17 05:46:55 +0100

    Initial release. Builds but the _preprocess does not yield the required result since csproj is in the exclude listing

---
+++ VsixConsoleAppTemplate/CustomWizard/Wizard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace CustomWizard
+{
+    public class Wizard : IWizard {
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            replacementsDictionary.Add("$companynamespace$", "Infomedia");
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            Debug.WriteLine("ShouldAddProjectItem");
+            return true;
+        }
+
+        public void RunFinished()
+        {
+            Debug.WriteLine("RunFinished");
+        }
+
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+            Debug.WriteLine("BeforeOpeningFile");
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+            Debug.WriteLine("ProjectItemFinishedGenerating");
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+            Debug.WriteLine("ProjectFinishedGenerating");
+        }
+    }
+}
